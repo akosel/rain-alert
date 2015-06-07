ad7a47103fec4c6e63efab0eec9956cabf2c8224
Aaron Kosel <a_kosel@yahoo.com>
2015-06-07 16:04:34 -0400

    Put lat and lng into config

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import forecastio
 import config
 
 api_key = config.API_KEY
-lat = 42.3076
-lng = -83.7398
+lat = config.LAT 
+lng = config.LNG
 
 forecast = forecastio.load_forecast(api_key, lat, lng)
 print forecast.hourly()
