057fe594e1bc1825a91692d0e1b571e42905cd14
Aaron Kosel <a_kosel@yahoo.com>
2015-06-07 18:15:13 -0400

    Check minute by minute.

---
+++ app.py
@@ -0,0 +1,5 @@
+import forecast_wrapper
+
+if __name__ == '__main__':
+    forecaster = forecast_wrapper.ForecastWrapper()
+    forecaster.willItRain()
--- forecast_wrapper.py
+++ forecast_wrapper.py
@@ -1,5 +1,6 @@
 import forecastio
 import config
+import datetime
 
 class ForecastWrapper(object):
     def __init__(self):
@@ -10,6 +11,12 @@ class ForecastWrapper(object):
 
     def willItRain(self):
         minutely_data = self.forecast.minutely().data
+        now = datetime.datetime.now()
 
         for minute in minutely_data:
-            print minute.d
+            if minute.d['precipProbability'] > .5:
+                then = datetime.datetime.fromtimestamp(minute.d['time'])
+                rain_start = (then - now)
+                minutes_from_now = rain_start.seconds / 60
+                print 'The odds are good in {0} minute(s))'.format(minutes_from_now)
+                print 'Dark Sky prediction {0}'.format(minute.d)
